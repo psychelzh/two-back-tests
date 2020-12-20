27328d7ca67a2202aaaf8e37c9d19e3f61cfa776
Liang Zhang <psychelzh@outlook.com>
2020-12-21 00:11:48 +0800

    🔧build(stimuli): make dot distinctive
    Dots are now more far away for cross now.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- src/prepare_stimuli_space.m
+++ src/prepare_stimuli_space.m
@@ -5,7 +5,7 @@
 % Group 2: y > x & y > 1 - x
 % Group 3: y > x & y < 1 - x
 % Group 4: y < x & y < 1 - x
-% further ensure x, y in the range of [0.1, 0.4] or [0.6, 0.9]
+% ensure they are not near the lines
 rng(20201220)
 num_stim_each_group = 21;
 num_blocks = 7;
@@ -16,30 +16,29 @@ for group = 1:num_group
     for i_stimuli = 1:num_stim_each_group
         while true
             coord_pos = rand(1, 2);
-            if any(coord_pos < 0.1 | ...
-                    (coord_pos > 0.4 & coord_pos < 0.6) | ...
-                    coord_pos > 0.9)
+            check_diags = [coord_pos(2) - coord_pos(1), coord_pos(2) - (1 - coord_pos(1))];
+            if any(abs(check_diags) < 0.1)
                 continue
             end
             switch group
                 case 1
                     group_note = "第一象限";
-                    if coord_pos(2) < coord_pos(1) && coord_pos(2) > 1 - coord_pos(1)
+                    if check_diags(1) < 0 && check_diags(2) > 0
                         break
                     end
                 case 2
                     group_note = "第二象限";
-                    if coord_pos(2) > coord_pos(1) && coord_pos(2) > 1 - coord_pos(1)
+                    if check_diags(1) > 0 && check_diags(2) > 0
                         break
                     end
                 case 3
                     group_note = "第三象限";
-                    if coord_pos(2) > coord_pos(1) && coord_pos(2) < 1 - coord_pos(1)
+                    if check_diags(1) > 0 && check_diags(2) < 0
                         break
                     end
                 case 4
                     group_note = "第四象限";
-                    if coord_pos(2) < coord_pos(1) && coord_pos(2) < 1 - coord_pos(1)
+                    if check_diags(1) < 0 && check_diags(2) < 0
                         break
                     end
             end
